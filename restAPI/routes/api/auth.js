bb6cfa6b7d6f884610b0a8f6a02e5d18b225fc3a
EetuHei <t8heee01@students.oamk.fi>
2019-10-20 18:45:50 +0300

    auth routes, working login and get route test

---
+++ restAPI/routes/api/auth.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const db = require("../../config/database/database");
+//POST /api/
+//login handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/failurelogin",
+    successMessage: "logged in Pog",
+    failureMessage: "failed to login pepehands"
+  })(req, res, next);
+});
+
+//GET /api/
+//get user by id only for testing
+router.get(
+  "/users/:id",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    db.query("SELECT id, username FROM users WHERE id = ?", [
+      req.params.id
+    ]).then(results => {
+      res.json(results);
+    });
+  }
+);
+
+module.exports = router;
