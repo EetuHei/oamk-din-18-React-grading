daccde0a5fef58ebe7f7b15926ba7db278065666
EetuHei <t8heee01@students.oamk.fi>
2019-10-21 15:43:46 +0300

    Merge branch 'devFrontend' into restAPI

--- electric-app/src/App.js
+++ electric-app/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import GoogleMap from './components/Map/Googlemap';
+import Header from './components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state= {
+
+    };
+  }
+
+  render()
+  {
+    return(
+      <Router>
+        <Header />
+        <Route path="/" exact component={GoogleMap} />
+      </Router>
+    );
+  }
 }
-
-export default App;
