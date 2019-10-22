d4ce8fe81f22f790bb648330a1735d859fa4465d
Riku Heikkinen <riku.heikkinen94@gmail.com>
2019-10-22 19:24:39 +0300

    Fixed the header so that the buttons work and added some styling on them. Also put apikey in json file so that I don't keep pushing apikey to public.

--- electric-app/src/components/Header/Header.js
+++ electric-app/src/components/Header/Header.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./header.css";
-import {Navbar, Nav, Form, Container} from 'react-bootstrap';
+import { Navbar, Nav, Form, Container, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
-    return(
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Nav>
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>Pricing</Nav.Link>
-                </Nav>
-                <div className="centerDiv">
-                <Container>
-                    <Form.Group>
-                        <Form.Label className="centerText">Search Charging Stations</Form.Label>
-                        <Form.Control type="text" placeholder="City..."></Form.Control>
-                    </Form.Group>
-                </Container>
-                </div>
-                <Nav>
-                    <Nav.Link>Login</Nav.Link>
-                    <Nav.Link>Sign Up</Nav.Link>
-                </Nav>
-            </Navbar>
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Nav>
+          <Link to="/">
+            <Button style={{ float: "left" }} className="btn btn-dark">
+              Home
+            </Button>
+          </Link>
+          <Link to="/Pricing">
+            <Button style={{ float: "left" }} className="btn btn-dark">
+              Pricing
+            </Button>
+          </Link>
+        </Nav>
+        <div className="centerDiv">
+          <Container>
+            <Form.Group>
+              <Form.Label className="centerText">
+                Search Charging Stations
+              </Form.Label>
+              <Form.Control type="text" placeholder="City..."></Form.Control>
+            </Form.Group>
+          </Container>
         </div>
-    );
+        <Nav>
+          <Nav.Link>Login</Nav.Link>
+          <Nav.Link>Sign Up</Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
+  );
 }
--- electric-app/src/components/Map/Googlemap.js
+++ electric-app/src/components/Map/Googlemap.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
-import data from './data.json';
-import { Button } from 'react-bootstrap';
-
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
+import data from "./data.json";
+import { Button } from "react-bootstrap";
+import api from "./api.json";
 
 const mapStyles = {
-  width: '100%',
-  height: '100%'
+  width: "100%",
+  height: "100%"
 };
 
 export class GoogleMap extends Component {
-
   state = {
     activeMarker: {},
     selectedPlace: {},
@@ -35,68 +34,70 @@ export class GoogleMap extends Component {
     }
   };
 
-
-  componentDidMount(){
-    fetch('/data')
-    .then(r => r.json())
-    .then(data => {
-      this.setState({ markers: data.markers});
-    });
+  componentDidMount() {
+    fetch("/data")
+      .then(r => r.json())
+      .then(data => {
+        this.setState({ markers: data.markers });
+      });
   }
 
   render() {
-
     return (
       <div>
-      <Map
-        google={this.props.google}
-        zoom={6}
-        style={mapStyles}
-        initialCenter={{
-         lat: 65.02,
-         lng: 25.41
-        }}
-      >
-
-        {
-        data.markers.map(marker => <Marker onClick={this.onMarkerClick}
-        name={marker.name} id={marker.id} position={{lat: marker.latitude, lng: marker.longitude}} 
-        icon={{url: 'https://cdn2.iconfinder.com/data/icons/bitsies/128/Lightning-128.png',
-        scaledSize: new window.google.maps.Size(40,40)}}
-        />)
-        }
-        {
-        data.paidmarkers.map(marker => <Marker onClick={this.onMarkerClick}
-        name={marker.name} id={marker.id} position={{lat: marker.latitude, lng: marker.longitude}} 
-        icon={{url: 'https://cdn2.iconfinder.com/data/icons/miscellaneous-41/47/Asset_10-128.png',
-        scaledSize: new window.google.maps.Size(25,40)}}
-        />)
-        }
-
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
+        <Map
+          google={this.props.google}
+          zoom={6}
+          style={mapStyles}
+          initialCenter={{
+            lat: 65.02,
+            lng: 25.41
+          }}
         >
-          <div>
+          {data.markers.map(marker => (
+            <Marker
+              onClick={this.onMarkerClick}
+              name={marker.name}
+              id={marker.id}
+              position={{ lat: marker.latitude, lng: marker.longitude }}
+              icon={{
+                url:
+                  "https://cdn2.iconfinder.com/data/icons/bitsies/128/Lightning-128.png",
+                scaledSize: new window.google.maps.Size(40, 40)
+              }}
+            />
+          ))}
+          {data.paidmarkers.map(marker => (
+            <Marker
+              onClick={this.onMarkerClick}
+              name={marker.name}
+              id={marker.id}
+              position={{ lat: marker.latitude, lng: marker.longitude }}
+              icon={{
+                url:
+                  "https://cdn2.iconfinder.com/data/icons/miscellaneous-41/47/Asset_10-128.png",
+                scaledSize: new window.google.maps.Size(25, 40)
+              }}
+            />
+          ))}
 
-            <h4>
-            {this.state.selectedPlace.name}
-            </h4>
-            <h4>
-            {this.state.selectedPlace.id}
-            </h4>
-            <Button>asd</Button>
-          </div>
-        </InfoWindow>
-
-      </Map>
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+              <h4>{this.state.selectedPlace.id}</h4>
+              <Button>asd</Button>
+            </div>
+          </InfoWindow>
+        </Map>
       </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'OAISJFOIAEJROIAJR OIJRAOWIJRAWOIPRJWAOIRJAWOIRJ'
+  apiKey: api.key
 })(GoogleMap);
-
