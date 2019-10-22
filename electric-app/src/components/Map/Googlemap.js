5e7aedfd41ebcf6ce4d0b96f356b1bc89c6e0830
EetuHei <t8heee01@students.oamk.fi>
2019-10-22 18:38:10 +0300

    Fixed pull errors

--- electric-app/src/components/Map/Googlemap.js
+++ electric-app/src/components/Map/Googlemap.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 import data from "./data.json";
-import api from "./api.json";
 import { Button } from "react-bootstrap";
-
+import api from "./api.json";
+ 
 const mapStyles = {
   width: "100%",
   height: "100%"
 };
-
+ 
 export class GoogleMap extends Component {
   state = {
     activeMarker: {},
@@ -17,14 +17,14 @@ export class GoogleMap extends Component {
     markers: [],
     paidmarkers: []
   };
-
+ 
   onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true
     });
-
+ 
   onClose = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
@@ -33,7 +33,7 @@ export class GoogleMap extends Component {
       });
     }
   };
-
+ 
   componentDidMount() {
     fetch("/data")
       .then(r => r.json())
@@ -41,39 +41,18 @@ export class GoogleMap extends Component {
         this.setState({ markers: data.markers });
       });
   }
-
+ 
   render() {
     return (
       <div>
-      <Map
-        google={this.props.google}
-        zoom={6}
-        style={mapStyles}
-        initialCenter={{
-         lat: 65.02,
-         lng: 25.41
-        }}
-      >
-
-        {
-        data.markers.map(marker => <Marker onClick={this.onMarkerClick}
-        name={marker.name} id={marker.id} position={{lat: marker.latitude, lng: marker.longitude}} 
-        icon={{url: 'https://cdn2.iconfinder.com/data/icons/bitsies/128/Lightning-128.png',
-        scaledSize: new window.google.maps.Size(40,40)}}
-        />)
-        }
-        {
-        data.paidmarkers.map(marker => <Marker onClick={this.onMarkerClick}
-        name={marker.name} id={marker.id} position={{lat: marker.latitude, lng: marker.longitude}} 
-        icon={{url: 'https://cdn2.iconfinder.com/data/icons/miscellaneous-41/47/Asset_10-128.png',
-        scaledSize: new window.google.maps.Size(25,40)}}
-        />)
-        }
-
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
+        <Map
+          google={this.props.google}
+          zoom={6}
+          style={mapStyles}
+          initialCenter={{
+            lat: 65.02,
+            lng: 25.41
+          }}
         >
           {data.markers.map(marker => (
             <Marker
@@ -83,12 +62,25 @@ export class GoogleMap extends Component {
               position={{ lat: marker.latitude, lng: marker.longitude }}
               icon={{
                 url:
-                  "https://cdn3.iconfinder.com/data/icons/transport-2-10/128/Electric-Charging-Station-Tesla-Energy-Eco-Power-512.png",
-                scaledSize: new window.google.maps.Size(25, 25)
+                  "https://cdn2.iconfinder.com/data/icons/bitsies/128/Lightning-128.png",
+                scaledSize: new window.google.maps.Size(40, 40)
               }}
             />
           ))}
-
+          {data.paidmarkers.map(marker => (
+            <Marker
+              onClick={this.onMarkerClick}
+              name={marker.name}
+              id={marker.id}
+              position={{ lat: marker.latitude, lng: marker.longitude }}
+              icon={{
+                url:
+                  "https://cdn2.iconfinder.com/data/icons/miscellaneous-41/47/Asset_10-128.png",
+                scaledSize: new window.google.maps.Size(25, 40)
+              }}
+            />
+          ))}
+ 
           <InfoWindow
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}
@@ -105,7 +97,7 @@ export class GoogleMap extends Component {
     );
   }
 }
-
+ 
 export default GoogleApiWrapper({
   apiKey: api.key
 })(GoogleMap);
