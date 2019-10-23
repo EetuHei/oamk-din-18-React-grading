62a6e5297c269132a15241467c6635eb8e387470
Riku Heikkinen <riku.heikkinen94@gmail.com>
2019-10-23 17:57:40 +0300

    Added functions to store user data and made some new routes.

--- electric-app/src/App.js
+++ electric-app/src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import GoogleMap from './components/Map/Googlemap';
-import Header from './components/Header/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import GoogleMap from "./components/Map/Googlemap";
+import Header from "./components/Header/Header";
+import Register from "./components/Register/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import data from "./components/Map/data.json";
 
 export default class App extends React.Component {
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
 
-    this.state= {
-
+    this.state = {
+      searchField: "",
+      userData: null,
+      items: data.items
     };
   }
 
-  render()
-  {
-    return(
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  storeUserData = (username, password, passwordConfirm) => {
+    this.setState({
+      userData: {
+        username,
+        password,
+        passwordConfirm
+      }
+    });
+  };
+
+  render() {
+    return (
       <Router>
         <Header />
-        <Route path="/" exact component={GoogleMap} />
+        {/* <Route path="/" exact component={GoogleMap} /> */}
+        <Route
+          path="/"
+          exact
+          render={routeProps => <GoogleMap userData={this.state.userData} />}
+        />
+        <Route
+          path="/register"
+          exact
+          render={routeProps => (
+            <Register storeUserData={this.storeUserData} {...routeProps} />
+          )}
+        />
       </Router>
     );
   }
